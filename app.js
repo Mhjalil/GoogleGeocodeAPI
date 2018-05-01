df9231fc62b24717ad1de87a013448beb64a9020
Marco Jalili <smhj63@gmail.com>
2018-05-01 17:07:14 +1000

    added address_components rows

--- app.js
+++ app.js
@@ -22,9 +22,20 @@ function geocode() {
     <li class="list-group-item">${formattedAddress}</li>
     </ul>
     `;
+
+    // Address components
+    let addressComponents = response.data.results[0].address_components;
+    let addressComponentsOutput = '<ul class="list-group">';
+    for (let i = 0; i < addressComponents.length; i++) {
+        addressComponentsOutput +=`
+        <li class="list-group-item"><strong>${addressComponents[i].types[0]}:</strong>${addressComponents[i].long_name}</li>
+        `;
+    }
+    addressComponentsOutput += '</ul>'
     
     // Output to app
     document.getElementById('formatted-address').innerHTML = formattedAddressOutput;
+    document.getElementById('address-components').innerHTML = addressComponentsOutput;
 })
 .then(function(error) {
     console.log(error);  
